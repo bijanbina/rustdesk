e5076f4bdafaa055a9d65aa37bf765be6a3bbad8
Bijan Binaee <bijanbina@gmail.com>
2023-09-07 08:14:39 +0200

    Update state_model.dart

--- flutter/lib/models/state_model.dart
+++ flutter/lib/models/state_model.dart
@@ -66,31 +66,28 @@ class StateGlobal {
 
   setFullscreen(bool v) {
     _fullscreen = true;
-    if (_fullscreen != v) {
-      _showTabBar.value = !_fullscreen;
-      _resizeEdgeSize.value = fullscreen
-          ? kFullScreenEdgeSize
-          : isMaximized.isTrue
-              ? kMaximizeEdgeSize
-              : kWindowEdgeSize;
-      print(
-          "fullscreen: $fullscreen, resizeEdgeSize: ${_resizeEdgeSize.value}");
-      _windowBorderWidth.value = fullscreen ? 0 : kWindowBorderWidth;
-      WindowController.fromWindowId(windowId)
-          .setFullscreen(_fullscreen)
-          .then((_) {
-        // https://github.com/leanflutter/window_manager/issues/131#issuecomment-1111587982
-        if (Platform.isWindows && !v) {
-          Future.delayed(Duration.zero, () async {
-            final frame =
-                await WindowController.fromWindowId(windowId).getFrame();
-            final newRect = Rect.fromLTWH(
-                frame.left, frame.top, frame.width + 1, frame.height + 1);
-            await WindowController.fromWindowId(windowId).setFrame(newRect);
-          });
-        }
-      });
-    }
+    _showTabBar.value = !_fullscreen;
+    _resizeEdgeSize.value = fullscreen
+        ? kFullScreenEdgeSize
+        : isMaximized.isTrue
+            ? kMaximizeEdgeSize
+            : kWindowEdgeSize;
+    print(
+        "fullscreen: $fullscreen, resizeEdgeSize: ${_resizeEdgeSize.value}");
+    _windowBorderWidth.value = 0;
+    WindowController.fromWindowId(windowId).setFullscreen(_fullscreen)
+        .then((_) {
+      // https://github.com/leanflutter/window_manager/issues/131#issuecomment-1111587982
+      if (Platform.isWindows && !v) {
+        Future.delayed(Duration.zero, () async {
+          final frame =
+              await WindowController.fromWindowId(windowId).getFrame();
+          final newRect = Rect.fromLTWH(
+              frame.left, frame.top, frame.width + 1, frame.height + 1);
+          await WindowController.fromWindowId(windowId).setFrame(newRect);
+        });
+      }
+    });
   }
 
   StateGlobal._();
